78348bdad923b73270b7f87639da4af7760763bc
michael ozeri <ozeri.mich@gmai.com>
2018-12-12 20:17:30 +0200

    TODO
    - free not working
    - resize main

--- ex3/Parallel.c
+++ ex3/Parallel.c
@@ -1,4 +1,67 @@
 #include "Parallel.h"
 
+Semaphore * CreateBufferQueueSemaphore(int outputBufferSize)
+{
+	Semaphore * sem = (Semaphore*)malloc(sizeof(Semaphore));
+	if (sem == NULL) {
+		error("creating semaphore struct");
+		return NULL;
+	}
+	//create semaphore with size of outputbuffer
+	sem->handle = CreateSemaphore(
+		NULL,
+		SEMAPHORE_INITIAL_VALUE,
+		outputBufferSize, "bufferQueueSemaphore");
+	if (sem->handle == NULL) {
+		error("creating semaphore handle");
+		return NULL;
+	}
 
+	return sem;
+}
 
+Mutex * CreateMutexAnchorArray(int maxNumber)
+{
+	Mutex* mutexAnchorArray = (Mutex*)malloc(sizeof(Mutex)*maxNumber);
+	if (mutexAnchorArray == NULL) {
+		error("allcating mem for mutexAnchorArray");
+		return NULL;
+	}
+	for (int i = 0; i < maxNumber; i++)
+	{
+		mutexAnchorArray[i].handle = CreateMutex(
+			NULL, // can be set to null from recitation
+			false, // thread calling create Mutex should be its initial owner?
+			NULL); // its possible to use null but we need to make sure not to lose handle
+		if (mutexAnchorArray[i].handle == NULL) {
+			error("creating mutex anchor array");
+			return NULL;
+		}
+		mutexAnchorArray[i].locked = 0;
+	}
+	return mutexAnchorArray;
+}
+
+BufferValue * CreateOutputBuffer(int outputBufferSize)
+{
+	BufferValue* output_buffer = (BufferValue*)malloc(sizeof(BufferValue)*outputBufferSize);
+	if (output_buffer == NULL) {
+		error("creating output_bufer memory");
+		return NULL;
+	}
+	for (int i = 0; i < outputBufferSize; i++) {
+		output_buffer[i].mutex = (Mutex*)malloc(sizeof(Mutex));
+		if (output_buffer[i].mutex == NULL) {
+			error("creating bufferd value mutex memory");
+			return NULL;
+		}
+		output_buffer[i].mutex->handle = CreateMutex(NULL, true, NULL);
+		if (output_buffer[i].mutex->handle == NULL) {
+			error("creating bufferd value mutex");
+			return NULL;
+		}
+		//init for marking as not aquired
+		output_buffer[i].aquired = 0;
+	}
+	return output_buffer;
+}
--- ex3/Parallel.h
+++ ex3/Parallel.h
@@ -3,6 +3,11 @@
 #include <windows.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <wchar.h>
+#include <tchar.h> 
+
+#define SEMAPHORE_INITIAL_VALUE 1
+#define SEMAPHORE_MAX_VALUE 1
 
 /*
 struct representing the mutex has a handle to the mutex
@@ -20,4 +25,41 @@ typedef struct Semaphore {
 	HANDLE handle;
 }Semaphore;
 
+/*
+* this is the struct that represents a value to insert into the output_buffer
+*/
+typedef struct BufferValue {
+	int a;
+	int b;
+	int c;
+	int n;
+	int m;
+	int aquired;
+	Mutex* mutex;
+}BufferValue;
 
+/*
+this function creates the semaphore for the output_buffer
+parameters:
+outputBufferSize - the max value for the semaphore
+returns:
+Semaphore* if sucess else NULL
+*/
+Semaphore* CreateBufferQueueSemaphore(int outputBufferSize);
+
+/*
+this function creates the anchor array.
+parameters:
+maxNumber - the array size
+returns:
+a pointer to the anchor array if success else NULL
+*/
+Mutex* CreateMutexAnchorArray(int maxNumber);
+
+/*
+this function creates the output buffer including mutexes inside each place
+parameters:
+outputBufferSize - the size of the buffer to create
+returns pointer to the buffer on sucess else NULL
+*/
+BufferValue* CreateOutputBuffer(int outputBufferSize);
